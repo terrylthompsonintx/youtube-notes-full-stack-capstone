944a70a5fc6ce4431b86ea14077d697f63f9a626
Terry Thompson <terrylthompsonintx@gmail.com>
2018-01-05 15:05:07 -0600

    Styled and created html with mock data.

---
+++ models/younote.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var youSchema = new mongoose.Schema({
+
+    videoName: {
+        type: String,
+        required: true
+    },
+    videoUr;: {
+        type: String,
+        required: true
+    },
+    videoDate: {
+        type: Array,
+        required: false
+    }
+
+
+
+
+
+});
+
+var recipe = mongoose.model('younote', younoteSchema);
+
+module.exports = recipe;
